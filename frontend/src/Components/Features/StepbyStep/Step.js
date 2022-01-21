99a42f273ca75905e2be02dabe23cb211ec9ae1a
Alexander Umgeher <xzeiroil@gmail.com>
2022-01-21 13:00:44 +0100

    added new style because its not soo nice

--- frontend/src/Components/Features/StepbyStep/Step.js
+++ frontend/src/Components/Features/StepbyStep/Step.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react';
 
 // import image
 import logo from '../../../Assets/img/MainSite/Backkurse.jpeg';
@@ -14,24 +14,24 @@ export default function Step({ number }) {
 
     return (
         <div className='step-card-container shadow bg-body p-3 mb-5'>
-            <div className='step-card-container-2'>
-                {/* <div class="h1 mb-3">
+            <div>
+{/* <div class="h1 mb-3">
                 <i class="bi bi-check2-circle"></i>
             </div> */}
-                <br />
+            <br />
 
-                <div>
-                    {/* <div>
-        <p className='step-amount'>{number}</p>
-    </div> */}
-                    <div class="col-md">
-                        <img src={logo} class="img-fluid stepimg" alt="" />
-                    </div>
+            <div>
+                {/* <div>
+                    <p className='step-amount'>{number}</p>
+                </div> */}
+                <div class="col-md">
+                    <img src={logo} class="img-fluid stepimg" alt="" />
                 </div>
-                <br />
-                <h3>
-                    {step.text}
-                </h3>
+           </div>
+            <br />
+            <h3>
+                {step.text}
+            </h3>
             </div>
         </div>
     )
